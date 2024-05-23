0240dcd0742cef4f22b68b70c4e2fb680f523371
ya.dvoynih <ya.dvoynih@tensor.ru>
2024-05-23 18:15:07 +0300

    https://tracker.yandex.ru/YARFOOSBALL-2 Настрока базы данных

---
+++ backend/src/models/__init__.py
@@ -0,0 +1,2 @@
+from .base_model import BaseModel
+from .player_model import PlayerModel
---
+++ backend/src/models/player_model.py
@@ -0,0 +1,11 @@
+from sqlalchemy import String
+from sqlalchemy.orm import Mapped, mapped_column
+
+from .base_model import BaseModel
+
+
+class PlayerModel(BaseModel):
+    __tablename__ = "players"
+
+    first_name: Mapped[str] = mapped_column(String(20), unique=False, nullable=False)
+    last_name: Mapped[str] = mapped_column(String(20), unique=False, nullable=False)
